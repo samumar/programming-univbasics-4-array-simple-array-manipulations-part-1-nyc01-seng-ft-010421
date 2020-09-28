fb74ef515095d5ffd0a44612ff19302387bbb968
samumar43 <samumar43@gmail.com>
2020-09-28 04:36:48 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  @deleted_string = array.pop
+end
+
+def pop_with_args(array)
+  @small_dogs = array.pop(2)
+end
+
+def using_shift(array)
+  @im_so_over_this_city = array.shift
+end
+
+def shift_with_args(array)
+  @brands_removed = array.shift(2)
+end
